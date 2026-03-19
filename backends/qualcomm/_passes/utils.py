d69272f896a51878da04179e3791071336aab22c
shewu <shewu@qti.qualcomm.com>
2026-03-19 11:21:05 +0800

    Qualcomm AI Engine Direct - Add fp16a8w quantization config Summary: - Add a pass `insert_cast_for_fp_act_quantized_weight.py` to cast fp32 -> fp16 due to constraint in QNN HTP - Add a test case to run conv2d and linear with fp16a8w

--- backends/qualcomm/_passes/utils.py
+++ backends/qualcomm/_passes/utils.py
@@ -79,6 +79,7 @@ def get_passes_dependency_for_capture_program():
         FixedLinearKeepDim,
         FoldQDQ,
         I64toI32,
+        InsertCastForFpActQuantizedWeight,
         LayoutTransform,
         RecomposePadMaxPool2d,
         RecomposePixelUnshuffle,
@@ -112,6 +113,7 @@ def get_passes_dependency_for_capture_program():
         FixedLinearKeepDim: [FoldQDQ],
         FoldQDQ: [AnnotateQuantAttrs, AnnotateStack, AnnotateUnbind],
         I64toI32: [RemoveRedundancy],
+        InsertCastForFpActQuantizedWeight: [FoldQDQ, LayoutTransform],
         LayoutTransform: [
             AnnotateQuantAttrs,
             ExpandBroadcastTensorShape,
