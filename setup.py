52fcba693e7fd78edbe422dfc38db07b378ad168
shewu <shewu@qti.qualcomm.com>
2026-09-22 17:07:43 +0800

    test

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# test
 
 """
 Simple check list from AllenNLP repo: https://github.com/allenai/allennlp/blob/main/setup.py
